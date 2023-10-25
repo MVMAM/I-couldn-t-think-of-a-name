e8d6e7d0760475f68052081f0d18e895d5e4025d
MMM <ediconpts@gmail.com>
2023-10-25 16:09:48 +0500

    CR from CRUD

--- IP/news/migrations/0001_initial.py
+++ IP/news/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Artiles',
+            name='Articles',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=50, verbose_name='Название')),
--- IP/news/views.py
+++ IP/news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Artiles
 
 
 def news_home(request):
-    return render(request, 'news_home.html')
+    news = Artiles.objects.order_by('-date')
+    return render(request, 'news_home.html', {'news':news})
